177d9d9f6b96a322713c2e88d2b4c650eb80d0db
John <fang97j@gmail.com>
2019-12-05 01:44:21 -0500

    Add more to Makefile, solve 5a

--- Makefile
+++ Makefile
@@ -1,11 +1,23 @@
 CXX = g++
-CXXFLAGS = -std=c++11 -Wall -g
+CXXFLAGS = -std=c++11 -Wall
 
 two: two.o intcode.o
 	$(CXX) $(CXXFLAGS) -o two two.o intcode.o
 
 two.o: two.cpp intcode.h
 	$(CXX) $(CXXFLAGS) -c two.cpp
+
+three: three.cpp
+	$(CXX) $(CXXFLAGS) -o three three.cpp
+
+four: four.cpp
+	$(CXX) $(CXXFLAGS) -o four four.cpp
+
+five: five.o intcode.o
+	$(CXX) $(CXXFLAGS) -o five five.o intcode.o
+
+five.o: five.cpp intcode.h
+	$(CXX) $(CXXFLAGS) -c five.cpp
 
 intcode.o: intcode.h
 
--- intcode.cpp
+++ intcode.cpp
@@ -14,62 +14,40 @@ int interpret(vector<int>& nums, int val, int mode) {
 
 int run(vector<int> nums) {
   int idx = 0;
+  int ctr = 0;
 
-  /*
-  cout << "nums" << endl;
-  for (int n : nums) {
-    cout << n << endl;
-  }
-  cout << "===" << endl;
-  */
   while (1) {
+    ctr += 1;
     int opcode = nums[idx] % 100;
     int modes = nums[idx] / 100;
     if (opcode == 99) {
       break;
     }
 
-    // cout << "opcode: " << opcode << endl;
+    int a, b, in;
+    a = interpret(nums, nums[idx + 1], modes % 10);
+    b = interpret(nums, nums[idx + 2], (modes/10) % 10);
 
-    int a, b, c;
     switch(opcode) {
       case 1:
       case 2:
-        a = interpret(nums, nums[idx + 1], modes % 10);
-        b = interpret(nums, nums[idx + 2], (modes/10) % 10);
-        c = nums[idx + 3];
-        /*
-        cout << "a: " << a << endl;
-        cout << "mode a: " << modes % 10 << endl;
-        cout << "b: " << b << endl;
-        cout << "mode b: " << (modes/10) % 10 << endl;
-        cout << "c: " << c << endl;
-        cout << "mode c: " << (modes/100) % 10 << endl;
-        */
-
-        nums[c] = (opcode == 1) ? a + b : a * b;
+        nums[nums[idx + 3]] = (opcode == 1) ? a + b : a * b;
         idx += 4;
         break;
 
       case 3:
         cout << "> ";
-        cin >> nums[nums[idx + 1]];
+        cin >> in;
+        nums[nums[idx + 1]] = in;
         idx += 2;
         break;
 
       case 4:
-        cout << nums[nums[idx + 1]];
+        cout << nums[nums[idx + 1]] << endl;
         idx += 2;
         break;
     }
   }
-  /*
-  cout << "NUMS" << endl;
-  for (int n : nums) {
-    cout << n << endl;
-  }
-  cout << "===" << endl;
-  */
   return nums[0];
 }
 
