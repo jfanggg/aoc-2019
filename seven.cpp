c9d007cba09db5da3cf46b11ed5bf4b34186e2ee
John <fang97j@gmail.com>
2019-12-07 03:10:38 -0500

    Clean up day 7

--- seven.cpp
+++ seven.cpp
@@ -10,14 +10,6 @@ char ctoi(int x) {
   return x - '0';
 }
 
-int build_num(vector<int> parts) {
-  string s = "";
-  for (int part : parts) {
-    s += to_string(part);
-  }
-  return stoi(s);
-}
-
 int main() {
   vector<int> program;
   string s;
@@ -41,26 +33,18 @@ int main() {
   int ans2 = 0;
   do {
     int x = 0;
-    State a(program), b(program), c(program), d(program), e(program);
+    vector<State> states(5, State(program));
 
     // pass in phase value first
-    a = run(a, {ctoi(settings[0])});
-    b = run(b, {ctoi(settings[1])});
-    c = run(c, {ctoi(settings[2])});
-    d = run(d, {ctoi(settings[3])});
-    e = run(e, {ctoi(settings[4])});
-
-    while (!e.terminated) {
-      a = run(a, {x});
-      x = a.output[a.output.size() - 1];
-      b = run(b, {x});
-      x = b.output[b.output.size() - 1];
-      c = run(c, {x});
-      x = c.output[c.output.size() - 1];
-      d = run(d, {x});
-      x = d.output[d.output.size() - 1];
-      e = run(e, {x});
-      x = e.output[e.output.size() - 1];
+    for (int i = 0; i < 5; i++) {
+      states[i] = run(states[i], {ctoi(settings[i])});
+    }
+    // then actually do feedback loop
+    while (!states[4].terminated) {
+      for (int i = 0; i < 5; i++) {
+        states[i] = run(states[i], {x});
+        x = states[i].output[0];
+      }
     }
     ans2 = max(ans2, x);
   } while(next_permutation(settings.begin(), settings.end()));
