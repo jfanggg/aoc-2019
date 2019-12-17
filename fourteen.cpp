25138e8deaa272c05fd788e1e1de6fe316f35b96
John <fang97j@gmail.com>
2019-12-17 15:42:11 +0800

    Day 14, Part 2

--- fourteen.cpp
+++ fourteen.cpp
@@ -6,17 +6,57 @@
 #include <vector>
 using namespace std;
 
+const long TRIL = 1E12;
 typedef pair<string, int> Requirement;  // chemical name, amount required
 
 struct Ingredient {
   vector<Requirement> requires;         // what chemical and amounts it requires
   unordered_set<string> produces;       // which ingredients require it
   int produce_amount;                   // how much you can produce at once
+  unordered_set<string> blockers;       // copy of produces
+
+  void reset_blockers() {
+    blockers = produces;
+  } 
 };
+
+long solve(unordered_map<string, Ingredient>& ingredients, long num_fuel) {
+  unordered_map<string, long> req_amounts;                 // chemical -> how much of it you must produce
+  for (auto& iter : ingredients) {
+    iter.second.reset_blockers();
+  }
+
+  queue<string> unblocked;
+  unblocked.push("FUEL");
+  req_amounts["FUEL"] = num_fuel;
+
+  while (1) {
+    string p_chemical = unblocked.front();
+    unblocked.pop();
+    if (p_chemical == "ORE") {
+      break;
+    }
+
+    Ingredient& p_ingredient = ingredients.at(p_chemical);
+    long p_req_amount = req_amounts.at(p_chemical);
+    long multiplier = ceil(p_req_amount / (float) p_ingredient.produce_amount);
+
+    for (Requirement req : p_ingredient.requires) {
+      string r_chemical = req.first;
+      req_amounts[r_chemical] += multiplier * req.second;
+      
+      Ingredient& r_ingredient = ingredients.at(r_chemical);
+      r_ingredient.blockers.erase(p_chemical);
+      if (r_ingredient.blockers.size() == 0) {
+        unblocked.push(r_chemical);
+      }
+    }
+  }
+  return req_amounts.at("ORE");
+}
 
 int main() {
   unordered_map<string, Ingredient> ingredients;          // chemical -> ingrdient info
-  unordered_map<string, int> req_amounts;                 // chemical -> how much of it you must produce
 
   int quantity;
   string chemical;
@@ -42,32 +82,17 @@ int main() {
       ingredients[r.first].produces.insert(chemical);
     }
   }
+  cout << "Part 1: " << solve(ingredients, 1) << endl;
 
-  queue<string> unblocked;
-  unblocked.push("FUEL");
-  req_amounts["FUEL"] = 1;
-
-  while (1) {
-    string p_chemical = unblocked.front();
-    unblocked.pop();
-    if (p_chemical == "ORE") {
-      break;
-    }
-
-    Ingredient& p_ingredient = ingredients.at(p_chemical);
-    int p_req_amount = req_amounts.at(p_chemical);
-    int multiplier = ceil(p_req_amount / (float) p_ingredient.produce_amount);
-
-    for (Requirement req : p_ingredient.requires) {
-      string r_chemical = req.first;
-      req_amounts[r_chemical] += multiplier * req.second;
-      
-      Ingredient& r_ingredient = ingredients.at(r_chemical);
-      r_ingredient.produces.erase(p_chemical);
-      if (r_ingredient.produces.size() == 0) {
-        unblocked.push(r_chemical);
-      }
+  long lo = 1, hi = 1E12, mid = 0;
+  while (lo < hi) {
+    mid = ceil((lo + hi) / 2.0);
+    long ores = solve(ingredients, mid);
+    if (ores > TRIL) {
+      hi = mid - 1;
+    } else {
+      lo = mid;
     }
   }
-  cout << "Part 1: " << req_amounts.at("ORE") << endl;
+  cout << "Part 2: " << lo << endl;
 }
