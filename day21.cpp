80c93820f67c40f7a9f9c68d9ae4b2fcf92f1c58
John <fang97j@gmail.com>
2020-05-12 10:22:58 -0400

    Solve Day 21 Part 2

--- day21.cpp
+++ day21.cpp
@@ -7,20 +7,40 @@ using namespace std;
 int main() {
   vector<long> inputs;
   auto memory = parse_input();
-  State state = {memory};
+  State state0 = {memory};
 
-  string input_string;
-  input_string += "NOT A J\n";
-  input_string += "NOT B T\n";
-  input_string += "OR T J\n";
-  input_string += "NOT C T\n";
-  input_string += "OR T J\n";
-  input_string += "AND D J\n";
-  input_string += "WALK\n";
+  string input_string1;
+  input_string1 += "NOT A J\n";
+  input_string1 += "NOT B T\n";
+  input_string1 += "OR T J\n";
+  input_string1 += "NOT C T\n";
+  input_string1 += "OR T J\n";
+  input_string1 += "AND D J\n"; // ((-A or -B or -C) and D)
+  input_string1 += "WALK\n";
 
-  for (char c : input_string) {
+  for (char c : input_string1) {
     inputs.push_back(c);
   }
-  state = run(state, inputs);
-  cout << "Part 1: " << state.output.back() << endl;
+  State state1 = run(state0, inputs);
+  cout << "Part 1: " << state1.output.back() << endl;
+
+  string input_string2;
+  input_string2 += "NOT A J\n";
+  input_string2 += "NOT B T\n";
+  input_string2 += "OR T J\n";  
+  input_string2 += "NOT C T\n";
+  input_string2 += "OR T J\n";  
+  input_string2 += "AND D J\n"; // ((-A or -B or -C) and D)
+  input_string2 += "NOT E T\n"; 
+  input_string2 += "NOT T T\n"; 
+  input_string2 += "OR H T\n"; 
+  input_string2 += "AND T J\n"; // (-A or -B or -C) and D and (E or H)
+  input_string2 += "RUN\n";
+
+  inputs.clear();
+  for (char c : input_string2) {
+    inputs.push_back(c);
+  }
+  State state2 = run(state0, inputs);
+  cout << "Part 2: " << state2.output.back() << endl;
 }
